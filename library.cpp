b1f5b66fd32aab00650d26639231758f6e1137f5
Anastasia <dushanova45@gmail.com>
2023-09-17 11:48:38 +0300

    - инициализация проекта

---
+++ library.cpp
@@ -0,0 +1,15 @@
+#include "library.h"
+#include "ui_library.h"
+
+Library::Library(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Library)
+{
+    ui->setupUi(this);
+}
+
+Library::~Library()
+{
+    delete ui;
+}
+
